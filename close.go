e2495834857a76a2715f9e59d035bdbbd56e4704
chyroc <chyroc@qq.com>
2023-02-15 13:20:39 +0800

    :sparkles: feat: close to save data

---
+++ close.go
@@ -0,0 +1,20 @@
+package icloudgo
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func (r *Client) Close() error {
+	if err := os.WriteFile(r.clientIDPath, []byte(r.ClientID), 0644); err != nil {
+		return err
+	}
+
+	if bs, _ := json.Marshal(r.SessionData); len(bs) > 0 {
+		if err := os.WriteFile(r.sessionDataPath, bs, 0644); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
