21d9b12627786c0dfa6bb82d8f4732f44cfb0426
chyroc <chyroc@qq.com>
2023-02-17 22:24:51 +0800

    Rename const.go to icloud.go and update content

--- icloud.go
+++ icloud.go
@@ -1,6 +1,27 @@
 package icloudgo
 
-import "github.com/chyroc/icloudgo/internal"
+import (
+	"github.com/chyroc/icloudgo/internal"
+)
+
+func New(option *ClientOption) (*Client, error) {
+	return internal.NewClient(option)
+}
+
+type (
+	TextGetter   func(appleID string) (string, error)
+	Client       = internal.Client
+	ClientOption = internal.ClientOption
+	Error        = internal.Error
+	PhotoAlbum   = internal.PhotoAlbum
+	PhotoAsset   = internal.PhotoAsset
+	PhotoService = internal.PhotoService
+)
+
+var (
+	ErrValidateCodeWrong = internal.ErrValidateCodeWrong
+	ErrPhotosIterateEnd  = internal.ErrPhotosIterateEnd
+)
 
 const (
 	AlbumNameAll             = internal.AlbumNameAll
